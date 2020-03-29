01c716cc28ef2a3c76e3a0d4ebfb816166873ebf
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-29 17:04:22 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -63,7 +63,7 @@ class InteractiveRecord
     SQL
     
     DB[:conn].execute(sql)
-    
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
     
   end 
   
