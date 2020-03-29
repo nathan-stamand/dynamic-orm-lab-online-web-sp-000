d1b2f3ce33349a4513f7268b055e60ba3ff6a7e4
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-29 17:03:27 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -62,6 +62,8 @@ class InteractiveRecord
     INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
     SQL
     
+    DB[:conn].execute(sql)
+    
   end 
   
 end
